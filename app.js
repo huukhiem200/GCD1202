0596cd37ded1a731ee17c2657facd0de971e6aea
Trí Tài <taintgcd210330@fpt.edu.vn>
2024-10-23 15:45:34 +0700

    hello

--- app.js
+++ app.js
@@ -3,23 +3,35 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var session = require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var adminRouter = require('./routes/admin');
+var adminRouter = require('./routes/admins');
 var app = express();
+// Define session setting
+var sess = {
+  secret: 'cloud computing app',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {maxAge: 600000},
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+// Add the session with sess setting into app
+app.use(session(sess));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/admin', adminRouter);
+app.use('/admins', adminRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- async_demo.js
+++
@@ -1,3 +0,0 @@
-console.log("I am first");
-setTimeout(() => {console.log("I am second")}, 5000);
-console.log("I am third");
